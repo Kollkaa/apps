ef591534563c6a9229e6582fd987976b465d097d
Mykolai <1kolia1@ukr.net>
2019-05-29 17:47:56 +0300

    heroku

--- src/main/java/spring/boot/Application.java
+++ src/main/java/spring/boot/Application.java
@@ -2,11 +2,26 @@ package spring.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        System.out.println("contunie");
+        try//
+        {
+            Bot bot=new Bot();
+            telegramBotsApi.registerBot(bot);
+
+
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
         SpringApplication.run(Application.class, args);
 
     }
