6de391b3020331d5424790c3ba52994eb46689e4
Mykolai <1kolia1@ukr.net>
2019-06-04 15:13:35 +0300

    heroku

--- src/main/java/spring/boot/Bot.java
+++ src/main/java/spring/boot/Bot.java
@@ -300,11 +300,13 @@ String art="";
                     break;
                 case "enter":
                     count_zakazov+=1;
-                    try {
+                  /*  try {
                         execute(sends("Ім'я Користувача : "+usere.getName()+"("+update.getMessage().getChat().getUserName()+")"+"\n"+"Замовлення №"+usere.getChatid(),chatid));
                     } catch (TelegramApiException e) {
                         e.printStackTrace();
                     }
+
+                   */
                     usere.setZakaz(usere.getZakaz()+1);
 
                     System.out.println("about enter");
